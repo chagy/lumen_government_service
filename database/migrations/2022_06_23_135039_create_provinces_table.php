7c7dca6e06f221eeddf92ab82850fcc25251b7f3
Chagy <chagy_x_@hotmail.com>
2022-06-23 20:52:39 +0700

    Migrate Province

---
+++ database/migrations/2022_06_23_135039_create_provinces_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->string('prov_name')->unique()->comment('ชื่อจังหวัด');
+            $table->string('prov_desc')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinces');
+    }
+};
