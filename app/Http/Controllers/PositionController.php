c0148d4e677530f76301b1fed257b03e2de8d12f
Chagy <chagy_x_@hotmail.com>
2022-07-03 20:01:43 +0700

    Position create

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Position;
 use Illuminate\Http\Request;
 use App\Http\Resources\PositionResource;
+use Illuminate\Support\Facades\Validator;
 
 class PositionController extends Controller
 {
@@ -19,6 +20,30 @@ class PositionController extends Controller
 
     public function show($position) {
         $position = Position::findOrFail($position);
+
+        return response()->json([
+            'success' => true,
+            'data' => new PositionResource($position)
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'posi_name' => 'required|unique:positions',
+            'posi_desc' => 'nullable'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $position = Position::create([
+            'posi_name' => $request->posi_name,
+            'posi_desc' => $request->posi_desc
+        ]);
 
         return response()->json([
             'success' => true,
